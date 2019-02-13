9f767c48be78421d5701a275bda7eee75926128d
Nitish <nitish@maropost.com>
2019-02-13 15:09:20 +0530

    Initial Commit

---
+++ PixelCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by maropost on 13/02/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
